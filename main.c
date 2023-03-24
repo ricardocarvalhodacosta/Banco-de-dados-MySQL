4c04d373f9b3830999976292d46ad898106dae5a
RIcardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-03-24 18:11:09 -0300

    Adicionado arquivo de cabeçalho
    Adicionei um arquivo de cabeçalho para deixar o código mais limpo. Criei funções para escolha no menu do programa.

--- main.c
+++ main.c
@@ -3,143 +3,62 @@
 #include <windows.h>
 #include <mysql.h>
 #include <locale.h>
-
-void erro(MYSQL *conexao)
-{
-    fprintf(stderr, "\n%s\n", mysql_error(conexao));
-    mysql_close(conexao);
-    exit(1);
-}
-
-MYSQL *__stdcall obterConexao()
-{
-    char *servidor = "127.0.0.1";
-    char *usuario = "root";
-    char *senha = "";
-    char *nomeBanco = "bancoSQL_C";
-
-    MYSQL *conexao = mysql_init(NULL);
-
-    if (!mysql_real_connect(conexao, servidor, usuario, senha, nomeBanco, 0, NULL, 0))
-    {
-        fprintf(stderr, "\n%s\n", mysql_error(conexao));
-        mysql_close(conexao);
-        exit(1);
-    }
-    else
-    {
-        printf("\nConexão realizada com sucesso!\n");
-        return conexao;
-    }
-}
-
-void inserir(MYSQL *conexao, char *nome, char *telefone, char *email )
-{
-    char query[100];
-    sprintf(query, "INSERT INTO clientes(nome, telefone, email) VALUES('%s', '%s', '%s');", nome, telefone, email);
-
-    if (mysql_query(conexao, query))
-    {
-        erro(conexao);
-    }
-    else
-    {
-        printf("\nDados inseridos com sucesso!\n");
-    }
-}
-
-void ler(MYSQL *conexao)
-{
-    MYSQL_RES *resultado;
-    MYSQL_ROW row;
-
-    if (mysql_query(conexao, "SELECT * FROM clientes"))
-    {
-        erro(conexao);
-    }
-
-    resultado = mysql_store_result(conexao);
-    if (resultado == NULL)
-    {
-        erro(conexao);
-    }
-
-    while ((row = mysql_fetch_row(resultado)) != NULL)
-    {
-        printf("\nNome: %s\n", row[0]);
-        printf("Telefone: %s\n", row[1]);
-        printf("Email: %s\n", row[2]);
-    }
-
-    mysql_free_result(resultado);
-}
-
-void atualizar(MYSQL *conexao, char email)
-{
-    char query[100];
-    sprintf(query, "UPDATE clientes SET email = %s WHERE email = %s;", email, email);
-
-    if (mysql_query(conexao, query))
-    {
-        erro(conexao);
-    }
-    else
-    {
-        printf("\nDados atualizados com sucesso!\n");
-    }
-}
-
-void apagar(MYSQL *conexao, char email)
-{
-    char query[100];
-    sprintf(query, "DELETE FROM clientes WHERE email = %s;", email);
-
-    if (mysql_query(conexao, query))
-    {
-        erro(conexao);
-    }
-    else
-    {
-        printf("\nDados apagados com sucesso!\n");
-    }
-}
+#include "header.h"
 
 int main()
 {
     setlocale(LC_ALL, "portuguese-brazilian");
     MYSQL *conexao = obterConexao();
-    char email[100];
+    char *email[100];
     char nome[100];
     char telefone[100];
     int i;
+    char menu;
 
-    /*printf("\n*** CADASTRO DE CLIENTES ***\n");
-    for (i = 0; i < 3; i++)
+    printf("Olá, o que você gostaria de fazer?\n");
+    printf("1. Adicionar novo usuário, 2. Ler os dados, 3. Alterar os dados, 4. Apagar dados;\n");
+    scanf("%c", &menu);
+
+    switch(menu)
     {
-        printf("%d - Email: ", i);
-        fflush(stdin);
-        gets (email);
-        printf("%d - Nome: ", i);
-        fflush(stdin);
-        gets(nome);
-        printf("%d - Telefone: ", i);
-        fflush(stdin);
-        gets(telefone);
-        inserir(conexao, nome, telefone, email);
-    }*/
+    case '1':
+        printf("\n*** CADASTRO DE CLIENTES ***\n");
+        for (i = 0; i < 3; i++)
+        {
+            printf("%d - Email: ", i);
+            fflush(stdin);
+            gets (email);
+            printf("%d - Nome: ", i);
+            fflush(stdin);
+            gets(nome);
+            printf("%d - Telefone: ", i);
+            fflush(stdin);
+            gets(telefone);
+            inserir(conexao, nome, telefone, email);
+        }
+        break;
 
-    printf("\n*** CLIENTES CADASTRADOS ***\n");
-    ler(conexao);
+    case '2':
+        printf("\n*** CLIENTES CADASTRADOS ***\n");
+        ler(conexao);
+        break;
 
-    atualizar(conexao, email);
+    case '3':
+        atualizar(conexao, email);
+        printf("\n*** CLIENTES CADASTRADOS ***\n");
+        ler(conexao);
+        break;
 
-    printf("\n*** CLIENTES CADASTRADOS ***\n");
-    ler(conexao);
+    case '4':
+        apagar(conexao, 11);
 
-    // apagar(conexao, 11);
+        printf("\n*** CLIENTES CADASTRADOS ***\n");
+        ler(conexao);
+        break;
 
-    // printf("\n*** CLIENTES CADASTRADOS ***\n");
-    // ler(conexao);
+    default:
+        break;
+    }
 
     mysql_close(conexao);
     return 0;
