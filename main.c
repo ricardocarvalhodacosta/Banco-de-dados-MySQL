2991b7487747d02e735749e9294df827c011af72
RIcardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-03-26 20:24:22 -0300

    Main otimizada
    Retirado as funções que estavam na main e levadas para o arquivo "config.c";

--- main.c
+++ main.c
@@ -9,56 +9,7 @@ int main()
 {
     setlocale(LC_ALL, "portuguese-brazilian");
     MYSQL *conexao = obterConexao();
-    char *email[100];
-    char nome[100];
-    char telefone[100];
-    int i;
-    char menu;
-
-    printf("Olá, o que você gostaria de fazer?\n");
-    printf("1. Adicionar novo usuário, 2. Ler os dados, 3. Alterar os dados, 4. Apagar dados;\n");
-    scanf("%c", &menu);
-
-    switch(menu)
-    {
-    case '1':
-        printf("\n*** CADASTRO DE CLIENTES ***\n");
-        for (i = 0; i < 3; i++)
-        {
-            printf("%d - Email: ", i);
-            fflush(stdin);
-            gets (email);
-            printf("%d - Nome: ", i);
-            fflush(stdin);
-            gets(nome);
-            printf("%d - Telefone: ", i);
-            fflush(stdin);
-            gets(telefone);
-            inserir(conexao, nome, telefone, email);
-        }
-        break;
-
-    case '2':
-        printf("\n*** CLIENTES CADASTRADOS ***\n");
-        ler(conexao);
-        break;
-
-    case '3':
-        atualizar(conexao, email);
-        printf("\n*** CLIENTES CADASTRADOS ***\n");
-        ler(conexao);
-        break;
-
-    case '4':
-        apagar(conexao, email);
-
-        printf("\n*** CLIENTES CADASTRADOS ***\n");
-        ler(conexao);
-        break;
-
-    default:
-        break;
-    }
+    setup();
 
     mysql_close(conexao);
     return 0;
