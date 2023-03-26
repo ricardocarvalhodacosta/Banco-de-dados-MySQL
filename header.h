6aaf6854e8b8a5b4b60c6186362f1558897a7df6
RIcardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-03-26 20:26:41 -0300

    Adicionado a variável "void setup();"
    Adiciona a variável "void setup();" onde encontram-se as configurações do menu do programa.

--- header.h
+++ header.h
@@ -1,11 +1,13 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
+
 void erro(MYSQL *conexao);
 MYSQL *__stdcall obterConexao();
+void setup();
 void inserir(MYSQL *conexao, char *nome, char *telefone, char *email );
 void ler(MYSQL *conexao);
-void atualizar(MYSQL *conexao, char email);
-void apagar(MYSQL *conexao, char email);
+void atualizar(MYSQL *conexao);
+void apagar(MYSQL *conexao);
 
 #endif // HEADER_H_INCLUDED
